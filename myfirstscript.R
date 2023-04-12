c697842d713b4a644efa33d241171540ab9f1e5d
ceccarelliaurora <130578037+ceccarelliaurora@users.noreply.github.com>
2023-04-12 17:24:18 +0200

    Create myfirstscript.R

---
+++ myfirstscript.R
@@ -0,0 +1,21 @@
+#Myfirst
+2+3
+#Assign
+a<-2+3
+a
+#example
+#function()
+sherika <-c (10, 50, 70, 90, 100)
+alessio
+alessio<-c (5,10,20,40,80)
+#plot
+plot(alessio,sherika)
+plot(alessio,sherika, cex=5, col="red")
+#pointcharacter
+plot(alessio,sherika, cex=5, col="red",pch=19)
+#labels
+plot(alessio,sherika, cex=5, col="red",pch=19, xlab="shark bite",ylab="gonorrhea bacterium")
+plot(alessio,sherika, cex=5, col="red",pch=19, xlab="shark bite",ylab="gonorrhea bacterium",cex.lab=2)
+plot(alessio,sherika, cex=5, col="red",pch=19, xlab="shark bite",ylab="gonorrhea bacterium",cex.lab=2, cex.axis=2, main="relationship between shark bite and gonorrhea")
+#correlation
+cor(alessio,sherika)
